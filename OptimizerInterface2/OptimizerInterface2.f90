e3bb1804e82c3ac199ea98cf6ad9720741e0a5d9
Geoff Groos <geoff_groos@msn.com>
2014-11-08 01:48:32 -0800

    doesn't like my lambda :(
    oh well, probably another referencing/dereferencing issue.

--- OptimizerInterface2/OptimizerInterface2.f90
+++ OptimizerInterface2/OptimizerInterface2.f90
@@ -1,16 +1,32 @@
 MODULE OptimizerInterface2
-    USE JaggedMat
     USE ISO_C_BINDING
     USE ISO_FORTRAN_ENV
 
     IMPLICIT NONE
+    
+    ABSTRACT INTERFACE
+        TYPE(C_PTR) FUNCTION evaluatorCallback(inputVector, length)&
+            BIND(C, name='evaluatorCallback')
+            USE, INTRINSIC :: ISO_C_BINDING
+            TYPE(C_PTR), INTENT(IN), VALUE :: inputVector
+            INTEGER(C_INT), INTENT(IN), VALUE :: length
+        END FUNCTION
+    END INTERFACE
 
     CONTAINS
     
     SUBROUTINE pauseOptimization(meh) BIND(C, name="pauseOptimization")
     !DEC$ ATTRIBUTES DLLEXPORT :: pauseOptimization
     LOGICAL(C_BOOL),INTENT(IN) :: meh
+        PRINT *,meh
+    END SUBROUTINE
     
+    SUBROUTINE acceptCallback(boolean, callback) BIND(C,name="acceptCallback")
+    !DEC$ ATTRIBUTES DLLEXPORT :: acceptCallback
+    LOGICAL(C_BOOL),INTENT(IN) :: boolean
+    PROCEDURE(evaluatorCallback), POINTER :: callback
+        CALL callback
+        PRINT *,boolean
     END SUBROUTINE
 
 END MODULE OptimizerInterface2
--- console test/Source.cpp
+++ console test/Source.cpp
@@ -3,8 +3,14 @@
 
 using namespace std;
 
+typedef double* (*PointEvaluator)(double*, int);
+
 extern "C"{
-	void pauseOptimization(bool);
+	//oh nifty, VS generates this like its documentation...
+	void pauseOptimization(bool*);
+
+	//callback is (double*, int) -> double*
+	void acceptCallback(bool, PointEvaluator);
 }
 
 int main()
@@ -12,6 +18,18 @@ int main()
 	cout << "running!" << endl;
 	system("PAUSE");
 
-	pauseOptimization(true);
-	cout << "did it!" << endl;
+	cout << "passing in a boolean: FORTRAN{" << endl;
+	bool aBool = true;
+	pauseOptimization(&aBool);
+	cout << "}" << endl;
+	system("PAUSE");
+
+	cout << "passing in a boolean and a NULL func ptr! FOTRAN{" << endl;
+	acceptCallback(true, [](double* point, int nCoords) -> double* { 
+		cout << "hello again from C++!" << endl;
+		return NULL;
+	});
+	cout << "}" << endl;
+
+	system("PAUSE");
 }
